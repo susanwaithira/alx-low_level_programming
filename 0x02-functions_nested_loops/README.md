3037cf24f90f2f8cb08fd557fbd959246d2ed7de
susanwaithira <susanwaithirawangechi@gmail.com>
2023-12-28 19:26:59 +0300

    Fp

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+My readme nested loops 
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - function that prints 10mtimes
+ *the numbers ,from 0 to 14 followed by a new
+ * line
+ * Return: 10 times of the numbers 0 up to 14
+ */
+void more_numbers(void)
+{
+int x, y;
+for (x = 0; x < 10; x++)
+{
+for (y = 0; y <= 14; y++)
+{
+if (y > 9)
+{
+_putchar((y / 10) + '0');
+}
+_putchar((y % 10) + '0');
+}
+_putchar('\n');
+}
+
+}
+
